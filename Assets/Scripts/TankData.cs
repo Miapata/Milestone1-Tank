6a2e5031b0a0cd3e25714548f08de79c7001027f
KNOWN-UNIVERSE\MIAPATA <miapata@uat.edu>
2018-09-12 16:24:10 -0700

    Added variables to TankData.cs

--- Assets/Scripts/TankData.cs
+++ Assets/Scripts/TankData.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TankData : MonoBehaviour {
+public class TankData : MonoBehaviour
+{
+    public float moveSpeed;
 
+    public float rotateSpeed;
 	// Use this for initialization
 	void Start () {
 		
