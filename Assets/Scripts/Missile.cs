e14cc6cb14a2ecc78693a42db27d3e683744556f
KNOWN-UNIVERSE\MIAPATA <miapata@uat.edu>
2018-09-12 16:17:52 -0700

    Added a lot of stuff: Explosions, Terrain, Tank, Crosshair, Environment Props (Rocks, Trees)

---
+++ Assets/Scripts/Missile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+
+    public Rigidbody rigidBody;
+    public GameObject explosion;
+    public float missileSpeed;
+	// Update is called once per frame
+    void Start()
+    {
+        Destroy(gameObject,10);
+        rigidBody.velocity = transform.right * missileSpeed;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.collider != null)
+        {
+            Instantiate(explosion, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Motor.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Motor : MonoBehaviour {
+
+
+    public Rigidbody missile;
+    public GameObject origin;
+    public CharacterController characterController;
+    public float speed;
+    public float rotateSpeed;
+    private float lastEvent;
+	// Use this for initialization
+	void Start () {
+            		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Here we get the vertical float 
+        float vertical= Input.GetAxis("Vertical");
+
+        //The character cntroller moves using the speed
+        characterController.SimpleMove(transform.forward*vertical*-speed);
+
+
+        //We get the horizontal speed 
+        float horizontal = Input.GetAxis("Horizontal");
+
+        //Using the speed we rotate the player
+        transform.Rotate(Vector3.up, horizontal*rotateSpeed);
+
+
+        //If the space key is pressed
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Rigidbody rocketInstance;
+            //Instantiate the missile at the gameobject origin
+            Instantiate(missile, origin.transform.position, Quaternion.LookRotation(transform.right));
+            
+
+        }
+	}
+}
