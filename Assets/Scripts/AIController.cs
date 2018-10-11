a1be73e27607aa7a3728294d9a67db17ebf5d487
KNOWN-UNIVERSE\miapata <MIAPATA@UAT.EDU>
2018-10-11 11:27:48 -0700

    no message

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -16,31 +16,66 @@ public class AIController : MonoBehaviour
     //current wapoint position
     public Vector3 currentWaypointPosition;
 
+    //The max FOV angle
+    public float maxAngle;
+
+
+    public float maxRadius;
+
+    public enum Personalities
+    {
+        //Once in sight, he will constantly pursue you
+        Chase,
+        //Once in sight, he will flee and once far enough he will fire once and start patrolling
+        Flee,
+        //Has better range and will fire from afar
+        Ranger,
+        //Only fires when he is out of your view
+        Sneak
+    }
+
     //index for the waypoint list
     public int wayPointIndex = 1;
 
-    //Direction
+
+    // This is the direction to the player's tanks
     private Vector3 direction;
+
+    //Angle 
+    private float angle;
+
+    //Direction
+    private Vector3 directionWaypoint;
 
     //Distance
     private float distance;
 
     //Offset
     private Vector3 offset;
+
+    // left angle to draw the FOV
+    private Vector3 leftAngle;
+
+    // right angle to draw the FOV
+    private Vector3 rightAngle;
+
     // Update is called once per frame
     void Update()
     {
         // sends message to Fire the missile
-        SendMessage("FireMissile"); 
-        
+        SendMessage("FireMissile");
+
         // Distance between the target and current transform
         distance = Vector3.Distance(transform.position, waypoints[wayPointIndex].transform.position);
-       
+
         //Move
         Move();
 
         //Rotate
         Rotate();
+
+        //Check if the player is in the sight of our AI tank
+        FOV();
 
     }
 
@@ -79,10 +114,10 @@ public class AIController : MonoBehaviour
     {
 
         // set Direction to current position - waypoint position
-        direction = transform.position - waypoints[wayPointIndex].transform.position;
+        directionWaypoint = transform.position - waypoints[wayPointIndex].transform.position;
 
         // Set rotation to direction
-        Quaternion rotation= Quaternion.LookRotation(direction);
+        Quaternion rotation = Quaternion.LookRotation(directionWaypoint);
 
         //Lerp our rotation to the desired rotation
         transform.rotation = Quaternion.Lerp(transform.rotation, rotation, tankData.rotateSpeed * Time.deltaTime);
@@ -101,6 +136,26 @@ public class AIController : MonoBehaviour
         {
             // Increase the index by one
             wayPointIndex++;
+        }
+    }
+
+
+    //tHE FOLD OF VIEW THAT WE USE FOR THE ai'S SIGHT
+    void FOV()
+    {
+
+        //Left angle to draw
+        leftAngle = Quaternion.AngleAxis(maxAngle / 2, transform.up) * -transform.forward * maxRadius;
+        rightAngle = Quaternion.AngleAxis(-maxAngle / 2, transform.up) * -transform.forward * maxRadius;
+
+        Debug.DrawRay(transform.position, leftAngle, Color.red);
+        Debug.DrawRay(transform.position, rightAngle, Color.red);
+        direction = (tank.transform.position - transform.position);
+        angle = Vector3.Angle(direction, -transform.forward);
+        if (angle < maxAngle * 0.5f)
+        {
+            Debug.DrawLine(transform.position, tank.transform.position);
+            print("Player in FOV");
         }
     }
 }
